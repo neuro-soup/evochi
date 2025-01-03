c27a1f6dfb6e2cf65896e1227504e8ea9afacced
lukasl-dev <git@lukasl.dev>
2025-01-03 03:36:32 +0100

    nix: make gymnasium rendering usable on wayland

--- nix/shell.nix
+++ nix/shell.nix
@@ -2,7 +2,7 @@
   pkgs ? import <nixpkgs> { },
 }:
 
-pkgs.mkShell {
+pkgs.mkShell rec {
   buildInputs = with pkgs; [
     # go
     go_1_23
@@ -29,5 +29,32 @@ pkgs.mkShell {
     python312Packages.gymnasium
     python312Packages.jaxtyping
     python312Packages.numpy
+    python312Packages.wandb
+
+    glib
+    glibc
+    zstd
+    wayland
+    glfw-wayland
+    qt5.full
+    libsForQt5.qt5.qtwayland
+    libGL
+    libGLU
+    gcc
+    zlib
+    qt5.qtbase
+    libsForQt5.qt5.qtwayland
+    SDL2
+    SDL2.dev
+    xorg.libX11
+    xorg.libX11.dev
+    xorg.libXext
+    xorg.libXext.dev
+    xorg.libXrandr
+    xorg.libXrandr.dev
   ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}
+  '';
 }

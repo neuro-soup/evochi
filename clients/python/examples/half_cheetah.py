642f8500113984f39fc06002cec8945cc09f8939
lukasl-dev <git@lukasl.dev>
2025-01-05 04:04:02 +0100

    client(py): refine changes

--- clients/python/examples/half_cheetah.py
+++ clients/python/examples/half_cheetah.py
@@ -1,14 +1,17 @@
 import asyncio
 from dataclasses import dataclass
+from typing import override
 
 import grpc.aio as grpc
 
-from evochi.v1 import Worker, Eval
+import evochi.v1 as evochi
 
 import numpy as np
 import gymnasium as gym
 import torch
 from torch import nn
+
+from evochi.v1.worker import Eval
 
 
 class SimpleMLP(nn.Module):
@@ -42,7 +45,7 @@ class State:
     params: torch.Tensor
 
 
-class HalfCheetah(Worker[State]):
+class HalfCheetah(evochi.Worker[State]):
     def __init__(
         self,
         channel: grpc.Channel,
@@ -51,22 +54,15 @@ class HalfCheetah(Worker[State]):
         vectorization_mode: gym.VectorizeMode = gym.VectorizeMode.SYNC,
         render: bool = False,
     ) -> None:
-        super().__init__(
-            channel=channel,
-            cores=cores,
-            initialize=lambda _: self._handle_init(),
-            evaluate=lambda _, epoch, slices: self._handle_eval(epoch, slices),
-            optimize=lambda _, epoch, rewards: self._handle_optim(epoch, rewards),
-        )
+        super().__init__(channel=channel, cores=cores)
         self._device = device
         self._vectorization_mode = vectorization_mode
         self._render = render
         self._noise: torch.Tensor | None = None
-        self._mlp: SimpleMLP = SimpleMLP(obs_dim=17, act_dim=6, hidden_dim=64).to(
-            self._device
-        )
+        self._mlp: SimpleMLP = SimpleMLP(obs_dim=17, act_dim=6, hidden_dim=64).to(self._device)
 
-    def _handle_init(self) -> State:
+    @override
+    def initialize(self) -> State:
         """Initialize shared information across all workers."""
         seed = np.random.randint(0, 2**32)
         return State(
@@ -77,14 +73,10 @@ class HalfCheetah(Worker[State]):
             params=nn.utils.parameters_to_vector(self._mlp.parameters()),
         )
 
+    @override
     @torch.inference_mode()
-    def _handle_eval(self, epoch: int, slices: list[slice]) -> list[Eval]:
+    def evaluate(self, epoch: int, slices: list[slice]) -> list[evochi.Eval]:
         """Evaluates the model by computing the rewards for each slice."""
-        if self._mlp is None:
-            self._mlp = self._construct_mlp(slices)
-        if self._noise is None:
-            self._generate_noise()
-
         total_width = sum(sl.stop - sl.start for sl in slices)
         env = gym.make_vec(
             "HalfCheetah-v5",
@@ -95,32 +87,20 @@ class HalfCheetah(Worker[State]):
         obs, _ = env.reset(seed=self.state.seed)
 
         dones = np.zeros(total_width, dtype=bool)
-        returns = np.zeros(total_width, dtype=np.float32)
+        total_rewards = np.zeros(total_width, dtype=np.float32)
 
         for _ in range(self.state.max_steps):
             actions = self._choose_actions(obs)
             obs, reward, terminations, truncations, _ = env.step(actions)
             dones = dones | terminations | truncations
-            returns += reward * ~dones
+            total_rewards += reward * ~dones
             if dones.all():
                 break
 
-        acc_rewards: list[tuple[slice, list[float]]] = []
-        offset = 0
-        for sl in slices:
-            rewards = returns[offset : offset + (sl.stop - sl.start)].tolist()
-            acc_rewards.append((sl, rewards))
-            offset += sl.stop - sl.start
+        return Eval.from_flat(slices, total_rewards.tolist())
 
-        return [
-            Eval(
-                slice=slice,
-                rewards=rewards,
-            )
-            for slice, rewards in acc_rewards
-        ]
-
-    def _handle_optim(self, epoch: int, raw_rewards: list[float]) -> State:
+    @override
+    def optimize(self, epoch: int, raw_rewards: list[float]) -> State:
         """Perform an optimization step."""
         assert self._mlp is not None, "Cannot optimize without model"
 
@@ -137,23 +117,18 @@ class HalfCheetah(Worker[State]):
         epsilon = self._noise
         pert = torch.sum(epsilon * rewards, dim=0)
 
-        # Update
         self.state.params = params + lr / (pop_size * noise_std) * pert
-        self._mlp = self._construct_mlp([])
-
-        # reset worker state
-        self._noise = None
-        self._rng = None
-
-        self._generate_noise()
         return self.state
+
+    @override
+    def on_state_change(self, state: State) -> None:
+        self._mlp = self._construct_mlp([])
+        self._generate_noise()
 
     def _choose_actions(self, obs: np.ndarray) -> np.ndarray:
         """Choose actions based on the given observation."""
         assert self._mlp is not None, "Cannot determine action without model"
-        actions: torch.Tensor = self._mlp(
-            torch.from_numpy(obs).float().to(self._device)
-        )
+        actions: torch.Tensor = self._mlp(torch.from_numpy(obs).float().to(self._device))
         return actions.detach().numpy()
 
     def _construct_mlp(self, slices: list[slice]) -> SimpleMLP:
@@ -161,9 +136,7 @@ class HalfCheetah(Worker[State]):
         perturbed."""
         mlp = self._mlp
         nn.utils.vector_to_parameters(
-            vec=self._perturbed_params(slices)
-            if len(slices) > 0
-            else self.state.params,
+            vec=self._perturbed_params(slices) if len(slices) > 0 else self.state.params,
             parameters=mlp.parameters(),
         )
         return mlp.to(self._device)
@@ -193,7 +166,7 @@ class HalfCheetah(Worker[State]):
 
 async def main() -> None:
     channel = grpc.insecure_channel("localhost:8080")
-    worker = HalfCheetah(channel=channel, cores=1, render=True)
+    worker = HalfCheetah(channel=channel, cores=5, render=False)
     await worker.start()
 
 
